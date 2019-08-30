20978a887c558b150e7d5158f2b714279ebff6d8
Marco Cardinali <mcardina@r000u08l03.marconi.cineca.it>
2019-08-30 12:57:15 +0200

    introduced the new funciont (still empty) for the abelian projection

---
+++ lib/gauge_conf_monopoles.c
@@ -0,0 +1,28 @@
+#ifndef GAUGE_CONF_MONO_C
+#define GAUGE_CONF_MONO_C
+
+#include"../include/macro.h"
+
+#include<complex.h>
+#ifdef OPENMP_MODE
+  #include<omp.h>
+#endif
+#include<stdlib.h>
+
+#include"../include/function_pointers.h"
+#include"../include/gauge_conf.h"
+#include"../include/gparam.h"
+
+
+void max_abelian_gauge(Gauge_Conf const * const GC,
+              Geometry const * const geo,
+              GParam const * const param)
+  {
+ 
+
+
+
+
+
+  }
+
